4406b7c26c2dd1d6ac960a3ef22592867cbacc67
mino0427 <59495081+mino0427@users.noreply.github.com>
2025-03-18 20:48:45 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -16,9 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: const HomeScreen(), // 시작 화면
     );
   }
 }
-
-//.\.venv\Scripts\Activate.ps1
